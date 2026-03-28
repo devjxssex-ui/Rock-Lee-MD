8f16b97d1e99685575543424f7793eed6e5d1393
Jxssex <devjxssex@gmail.com>
2026-03-27 23:10:22 -0600

    Actualizar Grupo•demote.js

--- plugins/Grupo•demote.js
+++ plugins/Grupo•demote.js
@@ -1,16 +1,22 @@
 const handler = async (m, { conn }) => {
-  if (!m.mentionedJid[0] && !m.quoted) {
-    let text = `*Mention or reply to the user you want to demote from admin.*`
-    return m.reply(text, m.chat, { mentions: conn.parseMention(text) })
-  }
+    if (!m.mentionedJid?.[0] && !m.quoted) {
+        let text = `🩻 *ROCK LEE BOT 🩻*\n\n*Menciona o responde al usuario que deseas degradar de administrador.*`
+        return m.reply(text, m.chat, { mentions: conn.parseMention(text) })
+    }
 
-  let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-  await conn.groupParticipantsUpdate(m.chat, [user], 'demote')
-  m.reply(`*✅ The user has been demoted from admin.*`)
+    let user = m.mentionedJid?.[0] ? m.mentionedJid[0] : m.quoted.sender
+    await conn.groupParticipantsUpdate(m.chat, [user], 'demote')
+    
+    m.reply(
+        `✅ *Usuario degradado de administrador!* 🩻\n\n• Usuario: @${user.split`@`[0]}\n\n` +
+        `⚡ Recuerda, ¡el camino del ninja requiere paciencia y disciplina!`,
+        null,
+        { mentions: [user] }
+    )
 }
 
 handler.help = ['demote']
-handler.tags = ['group']
+handler.tags = ['grupo']
 handler.command = ['demote']
 handler.group = true
 handler.admin = true
