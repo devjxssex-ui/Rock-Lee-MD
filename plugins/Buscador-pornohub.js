28c7574c9fc5b4fb1ec21da7ceb64eaaf8da832a
Jxssex <devjxssex@gmail.com>
2026-03-27 22:08:06 -0600

    Actualizar Buscador-pornohub.js

--- plugins/Buscador-pornohub.js
+++ plugins/Buscador-pornohub.js
@@ -1,61 +1,41 @@
-//Créditos del código DanielDiod 
-
-import cheerio from 'cheerio';
-import axios from 'axios';
-
 let handler = async (m, { conn, args, command, usedPrefix }) => {
-   if (!db.data.chats[m.chat].nsfw && m.isGroup) {
-    return m.reply('[❗] 𝐋𝐨𝐬 𝐜𝐨𝐦𝐚𝐧𝐝𝐨𝐬 +𝟏𝟖 𝐞𝐬𝐭𝐚́𝐧 𝐝𝐞𝐬𝐚𝐜𝐭𝐢𝐯𝐚𝐝𝐨𝐬 𝐞𝐧 𝐞𝐬𝐭𝐞 𝐠𝐫𝐮𝐩𝐨.\n> 𝐬𝐢 𝐞𝐬 𝐚𝐝𝐦𝐢𝐧 𝐲 𝐝𝐞𝐬𝐞𝐚 𝐚𝐜𝐭𝐢𝐯𝐚𝐫𝐥𝐨𝐬 𝐮𝐬𝐞 .enable nsfw');
-    }
+
+  if (!db.data.chats[m.chat].nsfw && m.isGroup) {
+    return m.reply('🚫 *Contenido +18 desactivado en este grupo*\n\n💪 Usa:\n.enable nsfw');
+  }
 
   if (!args[0]) {
-    return conn.reply(m.chat, `🍭 Por favor, ingresé la búsqueda que desea realizar en Pornhub.\nEjemplo: ${usedPrefix + command} con mi prima`, m);
+    return conn.reply(m.chat, 
+      `💪 *Necesito una búsqueda*\n\nEjemplo:\n${usedPrefix + command} anime`, 
+    m);
   }
 
   try {
     let searchResults = await searchPornhub(args[0]);
+
     let teks = searchResults.result.map((v, i) => 
-      `『 *P O R N H U B  - S E A R C H* 』
-🎞️ *Título:* ${v.title}
-🕒 *Duración:* ${v.duration}
+`🎬 *RESULTADO ${i + 1}*
+
+📌 *Título:* ${v.title}
+⏱ *Duración:* ${v.duration}
 👀 *Vistas:* ${v.views}
 🔗 *Link:* ${v.url}
----------------------------------------------------\n`).join('\n\n');
+
+━━━━━━━━━━━━━━━`).join('\n\n');
 
     if (searchResults.result.length === 0) {
-      teks = '🍭 No se encontraron resultados...';
+      teks = '❌ *No se encontraron resultados*';
     }
 
     conn.reply(m.chat, teks, m);
+
   } catch (e) {
-    return conn.reply(m.chat, `⚠️ Ocurrió un error: ${e.message}`, m);
+    return conn.reply(m.chat, `⚠️ Error: ${e.message}`, m);
   }
 };
 
 handler.tags = ['buscador']; 
 handler.help = ['pornhubsearch']; 
 handler.command = ['phsearch', 'pornhubsearch'];
+
 export default handler;
-
-async function searchPornhub(search) {
-  try {
-    const response = await axios.get(`https://www.pornhub.com/video/search?search=${search}`);
-    const html = response.data;
-    const $ = cheerio.load(html);
-    const result = [];
-
-    $('ul#videoSearchResult > li.pcVideoListItem').each(function(a, b) {
-      const _title = $(b).find('a').attr('title');
-      const _duration = $(b).find('var.duration').text().trim();
-      const _views = $(b).find('var.views').text().trim();
-      const _url = 'https://www.pornhub.com' + $(b).find('a').attr('href');
-      const hasil = { title: _title, duration: _duration, views: _views, url: _url };
-      result.push(hasil);
-    });
-
-    return { result };
-  } catch (error) {
-    console.error('⚠️ Ocurrió un error al buscar en Pornhub:', error);
-    return { result: [] };
-  }
-}
