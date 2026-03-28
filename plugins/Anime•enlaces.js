f7403be5f8770139157d90659aec3d3ada75b7f6
Jxssex <devjxssex@gmail.com>
2026-03-27 22:04:33 -0600

    Actualizar Anime•enlaces.js

--- plugins/Anime•enlaces.js
+++ plugins/Anime•enlaces.js
@@ -1,26 +1,28 @@
 var handler = async (m, { conn }) => {
   const message = `
-┏━━━━━━━━━━━━━━━━━┅
-┇                 *「 ANIME」*       
-┣━━━━━━━━━━━━━━━━━┅
-┃ ❖ https://kusonime.com
-┃ ❖ https://huntersekaisub.blogspot.com
-┃ ❖ https://riie.jp
-┃ ❖ https://m.meownime.ai
-┃ ❖ https://meownime.ltd
-┃ ❖ https://nimegami.id
-┃ ❖ https://animekompi.cam
-┃ ❖ https://nontonanimeid.top
-┃ ❖ https://kazefuri.vip
-┃ ❖ https://pendekarsubs.us
-┃ ❖ https://myanimelist.net
-┗━━━━━━━━━━━━━━━━┅
+💪🔥 *ENTRENAMIENTO OTAKU ACTIVADO* 🔥💪
+
+┏━━━━━━━━━━━━━━━━━━
+┃ 🥋 *FUENTES DE PODER ANIME*
+┣━━━━━━━━━━━━━━━━━━
+┃ ⚡ https://kusonime.com
+┃ ⚡ https://huntersekaisub.blogspot.com
+┃ ⚡ https://riie.jp
+┃ ⚡ https://m.meownime.ai
+┃ ⚡ https://meownime.ltd
+┃ ⚡ https://nimegami.id
+┃ ⚡ https://animekompi.cam
+┃ ⚡ https://nontonanimeid.top
+┃ ⚡ https://kazefuri.vip
+┃ ⚡ https://pendekarsubs.us
+┃ ⚡ https://myanimelist.net
+┗━━━━━━━━━━━━━━━━━━
+
+🔥 *¡ENTRENA TU PODER VIENDO ANIME!* 
+💥 *¡EL ESFUERZO TAMBIÉN ES SER OTAKU!*
 `;
 
- 
   const rcanal = m.quoted || m;
-
-  
   await conn.reply(m.chat, message, rcanal);
 };
 
